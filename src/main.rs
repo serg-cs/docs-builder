1f8ec66ca42032f70718fa081bb76ee9de47390b
serg-cs <sergiocastanosanchez@icloud.com>
2025-05-18 10:41:41 +0200

    First version arguments processing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::env::args;
+use std::fs::create_dir_all;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    // Confirm that the necessary parameters are present
+    let args: Vec<String> = args().collect();
+    if args.len() < 3 {
+        eprintln!("Not enough parameters provided.\nExpected: <data_directory_path> <deposit_html_path>");
+        std::process::exit(2);
+    }
+
+    // Define paths from parameters
+    let data_directory_path = Path::new(&args[1]);
+    let deposit_html_path = Path::new(&args[2]);
+
+    // Confirm paths are valid
+    if !data_directory_path.is_dir() {
+        eprintln!("Path provided for data directory is not valid.");
+        std::process::exit(2);
+    } else if create_dir_all(deposit_html_path).is_err() {
+        eprintln!("Directory to deposit HTMl not found and creation failed.");
+        std::process::exit(1);
+    }
+
+    println!("Program worked!\nData directory path: \t{}\nDeposit HTML path: \t{}",
+             data_directory_path.canonicalize().unwrap().display(), deposit_html_path.canonicalize().unwrap().display());
 }
