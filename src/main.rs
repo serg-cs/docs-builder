a49ef2071acd2013d773fc781502d6f45b7785f3
serg-cs <sergiocastanosanchez@icloud.com>
2025-05-18 10:44:04 +0200

    Get markdown files from data directory

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env::args;
-use std::fs::create_dir_all;
-use std::path::Path;
+use std::fs::{create_dir_all, read_dir};
+use std::path::{Path, PathBuf};
 
 fn main() {
     // Confirm that the necessary parameters are present
@@ -23,6 +23,20 @@ fn main() {
         std::process::exit(1);
     }
 
-    println!("Program worked!\nData directory path: \t{}\nDeposit HTML path: \t{}",
+    println!("Initialization worked!\nData directory path: \t{}\nDeposit HTML path: \t{}",
              data_directory_path.canonicalize().unwrap().display(), deposit_html_path.canonicalize().unwrap().display());
+    
+    _ = get_markdown_files_in_data(data_directory_path);
+}
+
+fn get_markdown_files_in_data(directory: &Path) -> Vec<PathBuf> {
+    let mut markdown_files = Vec::new();
+    for file in read_dir(directory).unwrap() {
+        let file = file.unwrap();
+        if file.path().to_str().unwrap().ends_with(".md") | file.path().to_str().unwrap().ends_with(".markdown") {
+            markdown_files.push(file.path());
+        }
+    }
+
+    markdown_files
 }
